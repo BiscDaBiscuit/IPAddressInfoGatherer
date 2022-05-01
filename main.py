110469533dc63e4386ab9e0f0246400c72d0a866
Bisc <80872017+BiscDaBiscuit@users.noreply.github.com>
2022-04-30 20:57:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 #Or Run `pip install request` if needed.
 import requests
 
-## single ip request
-# response = requests.get("http://ip-api.com/json/24.48.0.1").json()
-#
-# print(response['lat'])
-# print(response['lon'])
-
-# batch ip request
-
 ip = input("Insert the ip you would like to get information on! (Ex: 208.80.152.201)\n\n")
 
 print()
